8347df9e2037a309f9fe1fea333d65d7fa4c1249
wtypmah24 <wtypmah24@gmail.com>
2023-11-13 12:38:07 +0100

    09.11 HW апдейт

--- src/main/java/org/HW_091123/task_1/RandomNumbers.java
+++ src/main/java/org/HW_091123/task_1/RandomNumbers.java
@@ -8,6 +8,11 @@ public class RandomNumbers {
      * Метод должен возвращать случайное нечётное число из диапазона от -100 до 100.
      *  Напишите тест для проверки метода. Сделайте так,
      * чтобы в тесте объект Random выдавал набор ожидаемых чисел, а не случайных.*/
+
+    /**
+     *
+     * ссылка на тесты https://github.com/wtypmah24/Java-Pro_HW2/blob/main/src/test/java/org/HW_091123/task_1/RandomNumbersTest.java
+     */
     public int getRandomEvenInteger(Random random) {
         if (random == null) throw new IllegalArgumentException("You have provided empty argument");
         int randomNUmber = random.nextInt(-100, 99);
